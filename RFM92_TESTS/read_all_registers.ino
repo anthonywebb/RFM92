6b4eebffef6ed7785fee11146701edea5df88185
Anthony Webb <anthony.webb@gmail.com>
2014-01-24 19:12:15 -0700

    Update read_all_registers.ino
    Option to set LoRa mode with 2

--- RFM92_TESTS/read_all_registers.ino
+++ RFM92_TESTS/read_all_registers.ino
@@ -25,6 +25,10 @@ void loop() {
       if(content == "1"){
         Serial.println("Reading all registers");
         readAllRegs();
+      } 
+      else if(content == "2"){
+        Serial.println("Entering LoRa Mode");
+        setLoRaMode()
       }
       content = "";
     } 
@@ -32,16 +36,15 @@ void loop() {
 }
 
 /////////////////////////////////////
-//    Method:   Read Register
+//    Method:   Write Register
 //////////////////////////////////////
 
-byte readRegister(byte addr)
+void writeRegister(byte addr, byte value)
 {
   select();
-  SPI.transfer(addr & 0x7F);
-  byte regval = SPI.transfer(0);
+  SPI.transfer(addr | 0x80); // OR address with 10000000 to indicate write enable;
+  SPI.transfer(value);
   unselect();
-  return regval;
 }
 
 /////////////////////////////////////
@@ -80,4 +83,61 @@ void readAllRegs( )
     Serial.print(" - ");
     Serial.println(regVal,BIN);
   }
+}
+
+/////////////////////////////////////
+//    Method:   Change the mode
+//////////////////////////////////////
+void setMode(byte newMode)
+{
+  if(newMode == currentMode)
+    return;  
+  
+  switch (newMode) 
+  {
+    case RF92_MODE_RX_CONTINUOS:
+      writeRegister(REG_OPMODE, newMode);
+      currentMode = newMode; 
+      Serial.println("Changing to Receive Continous Mode");
+      break;
+    case RF92_MODE_TX:
+      writeRegister(REG_OPMODE, newMode);
+      currentMode = newMode; 
+      Serial.println("Changing to Transmit Mode");
+      break;
+    case RF92_MODE_SLEEP:
+      writeRegister(REG_OPMODE, newMode);
+      currentMode = newMode; 
+      Serial.println("Changing to Sleep Mode"); 
+      break;
+    case RF92_MODE_STANDBY:
+      writeRegister(REG_OPMODE, newMode);
+      currentMode = newMode; 
+      Serial.println("Changing to Standby Mode");
+      break;
+    default: return;
+  } 
+  
+  if(newMode != RF92_MODE_SLEEP){
+    while(digitalRead(dio5) == 0)
+    {
+      Serial.print("z");
+    } 
+  }
+  
+  Serial.println(" Mode Change Done");
+  return;
+}
+
+/////////////////////////////////////
+//    Method:   Enable LoRa mode
+//////////////////////////////////////
+void setLoRaMode()
+{
+  Serial.println("Setting LoRa Mode");
+  setMode(RF92_MODE_SLEEP);
+  writeRegister(REG_OPMODE,0x80);
+   
+  Serial.println("LoRa Mode Set");
+  return;
 }
